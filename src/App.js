938087b8229bb1bd1fa4e50eef7286517bcc73ec
IsmagilovRF <ledohod@icloud.com>
2021-02-13 21:52:13 +0300

    version 1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from './components/Container';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Container color="#129067" />
+    <p>learn react</p>
+    </div>
+  );
+}
+
+export default App;
